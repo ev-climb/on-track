5c97c5c971c38e1219ff7095a094c2ebe25d0fd8
evgeny <evseev.develop@gmail.com>
2023-08-29 17:46:52 +0300

    in progress

--- src/components/TimelineStopwatch.vue
+++ src/components/TimelineStopwatch.vue
@@ -1,32 +1,64 @@
 <template>
   <div class="flex w-full gap-2">
-    <BaseButton :type="BUTTON_TYPE_DANGER">
+    <BaseButton :type="BUTTON_TYPE_DANGER" @click="reset" :disabled="!seconds">
       <ArrowPathIcon class="h-8" />
     </BaseButton>
     <div class="flex flex-grow items-center rounded bg-gray-100 px-2 font-mono text-3xl">
       {{ formatSeconds(seconds) }}
     </div>
-    <BaseButton :type="BUTTON_TYPE_WARNING">
+    <BaseButton v-if="isRunning" :type="BUTTON_TYPE_WARNING" @click="stop">
       <PauseIcon class="h-8" />
     </BaseButton>
-    <BaseButton :type="BUTTON_TYPE_SUCSESS">
+    <BaseButton v-else :type="BUTTON_TYPE_SUCSESS" @click="start" :disabled="isStartButtonDisabled">
       <PlayIcon class="h-8" />
     </BaseButton>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { ArrowPathIcon, PauseIcon, PlayIcon } from '@heroicons/vue/24/outline'
-import { BUTTON_TYPE_DANGER, BUTTON_TYPE_WARNING, BUTTON_TYPE_SUCSESS } from '../constants'
+import {
+  BUTTON_TYPE_DANGER,
+  BUTTON_TYPE_WARNING,
+  BUTTON_TYPE_SUCSESS,
+  MILLISECONDS_IN_SECOND
+} from '../constants'
 import { formatSeconds } from '../functions'
-import { isNumber } from '../validators'
+import { isHourValid, isNumber } from '../validators'
 import BaseButton from './BaseButton.vue'
 
-defineProps({
+const props = defineProps({
   seconds: {
     default: 0,
     type: Number,
     validator: isNumber
+  },
+  hour: {
+    required: true,
+    type: Number,
+    validator: isHourValid
   }
 })
+
+const seconds = ref(props.seconds)
+const isRunning = ref(false)
+const isStartButtonDisabled = props.hour !== new Date().getHours()
+
+function start() {
+  isRunning.value = setInterval(() => {
+    seconds.value++
+  }, MILLISECONDS_IN_SECOND)
+}
+
+function stop() {
+  clearInterval(isRunning.value)
+
+  isRunning.value = false
+}
+
+function reset() {
+  stop()
+  seconds.value = 0
+}
 </script>
